0f84418b55f64e4b6e9252ed99cddb53c77de63d
alxgb_dev <hey@alxgb.dev>
2024-06-01 03:14:40 +0200

    chore: cache node modules during workflow execution

--- release.config.cjs
+++ release.config.cjs
@@ -6,6 +6,25 @@ module.exports = {
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
-    '@semantic-release/git',
+     [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md', 'dist/**/*'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        pushTo: 'origin',
+        branch: 'release',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist/**/*',
+        successComment: false,
+        failComment: false,
+        labels: ['automated release'],
+        addReleases: 'bottom',
+      },
+    ],
   ],
 };
+
